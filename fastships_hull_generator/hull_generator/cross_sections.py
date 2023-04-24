57686e788fc7be9047ed0861ffc5b302bd83b37a
johnmartingodo <john.martin.kleven.godo@gmail.com>
2023-04-24 11:36:21 +0200

    Modified licensing header

--- fastships_hull_generator/hull_generator/cross_sections.py
+++ fastships_hull_generator/hull_generator/cross_sections.py
@@ -1,6 +1,19 @@
-# © 2023, NTNU
-# Author: John Martin Kleven Godø <john.martin.godo@ntnu.no>
-# This code is licenced under the GNU General Public License v3.0
+# This file is part of fastships_hull_generator, a tool for generating demihulls for slender catamarans
+# Copyright (C) 2023 NTNU
+# Author: John Martin Kleven Godø <john.martin.godo@ntnu.no;john.martin.kleven.godo@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
 from scipy.special import gamma as gamma_func
